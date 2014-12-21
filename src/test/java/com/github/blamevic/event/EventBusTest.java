f2e046cd2c60822cfcd3fd9aa9008570a5aa8493
RX14 <chris@rx14.co.uk>
2014-12-21 23:50:47 +0000

    Add more tests

--- src/test/java/com/github/blamevic/event/EventBusTest.java
+++ src/test/java/com/github/blamevic/event/EventBusTest.java
@@ -62,4 +62,90 @@ public class EventBusTest
 
         assertFalse("Events should not be delivered if no matchers match", testNonDeliveryHasRun);
     }
+
+    public boolean testMultipleMatchersHasRun = false;
+
+    @Test
+    public void testMultipleMatchers()
+    {
+
+        EventBus bus = new EventBus();
+        bus.processors.add(new IEventProcessor()
+        {
+            @Override
+            public List<IEventMatcher> getEventMatchers()
+            {
+                return Arrays.asList(event -> false, event -> true);
+            }
+
+            @Override
+            public void processEvent(IEvent event, List<IEventMatcher> matchedMatchers)
+            {
+                testMultipleMatchersHasRun = true;
+            }
+        });
+
+        bus.processEvent(new IEvent(){});
+
+        assertTrue("Events should be delivered if just one matcher is true", testMultipleMatchersHasRun);
+    }
+
+    public boolean testMultipleProcessorsHasRun = false;
+    public boolean testMultipleProcessorsHasRun2 = false;
+    public boolean testMultipleProcessorsHasRun3 = false;
+
+    @Test
+    public void testMultipleProcessors()
+    {
+        EventBus bus = new EventBus();
+
+        bus.processors.add(new IEventProcessor()
+        {
+            @Override
+            public List<IEventMatcher> getEventMatchers()
+            {
+                return Arrays.asList(event -> true, event -> true);
+            }
+
+            @Override
+            public void processEvent(IEvent event, List<IEventMatcher> matchedMatchers)
+            {
+                testMultipleProcessorsHasRun = true;
+            }
+        });
+
+        bus.processors.add(new IEventProcessor()
+        {
+            @Override
+            public List<IEventMatcher> getEventMatchers()
+            {
+                return Arrays.asList(event -> true, event -> false);
+            }
+
+            @Override
+            public void processEvent(IEvent event, List<IEventMatcher> matchedMatchers)
+            {
+                testMultipleProcessorsHasRun2 = true;
+            }
+        });
+
+        bus.processors.add(new IEventProcessor()
+        {
+            @Override
+            public List<IEventMatcher> getEventMatchers()
+            {
+                return Arrays.asList(event -> false, event -> false);
+            }
+
+            @Override
+            public void processEvent(IEvent event, List<IEventMatcher> matchedMatchers)
+            {
+                testMultipleProcessorsHasRun3 = true;
+            }
+        });
+
+        bus.processEvent(new IEvent(){});
+
+        assertTrue("Having multiple processors should be supported", testMultipleProcessorsHasRun && testMultipleProcessorsHasRun2 && !testMultipleProcessorsHasRun3);
+    }
 }
